c88281b89e11e444fe84e96b2e57127cdb1d98c7
Haluk Yıldız <halukyildiz@Haluk-MacBook-Air.local>
2020-09-08 21:37:31 +0300

    project finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,43 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
-  return <Layout></Layout>
+export default ({ data }) => {
+  console.log(data)
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+  return (
+    <Layout>
+      <Hero />
+      <Services />
+      <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        id
+        title
+        description
+        github
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 // ...GatsbyImageSharpFluid
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,8 +4,43 @@ import { graphql } from "gatsby"
 import Projects from "../components/Projects"
 // ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="projects-page">
+        <Projects projects={projects} title="all projects" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        id
+        title
+        description
+        github
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
